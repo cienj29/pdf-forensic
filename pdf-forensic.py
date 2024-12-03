96ba5aea1eddad60292e2d40abb2f8b1be40a737
Jayde <jaydecien29@gmail.com>
2024-12-02 21:45:42 -0800

    Update pdf-forensic.py
    updated from python2 to python3
    
    next: expand

--- pdf-forensic.py
+++ pdf-forensic.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 import PyPDF2
-import optparse
+import argparse
 from PyPDF2 import PdfFileReader
 
 def banner():
-	print '##########################################'
-	print '##                                      ##'
-	print '##      Author: Sazzad Ovi              ##'
-	print '##                                      ##'
-	print '##           XOVIM001                   ##'
-	print '##                                      ##'
-	print '##                                      ##'
-	print '##########################################'
+	print ('##########################################')
+	print ('##                                      ##')
+	print ('##      Author: Sazzad Ovi              ##')
+	print ('##                                      ##')
+	print ('##           XOVIM001                   ##')
+	print ('##                                      ##')
+	print ('##                                      ##')
+	print ('##########################################')
 
 banner()
 
 def printMeta(filename):
-	pdfFile = PdfFileReader(file(filename, 'rb'))
+	pdfFile = PdfFileReader(open(filename, 'rb'))
 	docInfo = pdfFile.getDocumentInfo()
 	print('[*] PDF MetaData for: ' + str(filename))
 	for metaItem in docInfo:
 		print('[+] ' + metaItem + ': ' + docInfo[metaItem])
 
 def main():
-	parser = optparse.OptionParser('usage program: -F <PDF file name>')
+	parser = oargparse.ArgumentParser('usage program: -F <PDF file name>')
 	parser.add_option('-F', dest='fileName', type='string',\
 		help='Please specify the PDF File name')
 	(options, args) = parser.parse_args()
