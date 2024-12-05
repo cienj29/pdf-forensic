9cd9a11140b4d8fbca3b19d1a7a96011b8b321eb
Jayde <jaydecien29@gmail.com>
2024-12-04 21:31:26 -0800

    Update pdf-forensic.py

--- pdf-forensic.py
+++ pdf-forensic.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
-import PyPDF2
+#!/usr/bin/env python3
+
 import argparse
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfReader
 
 def banner():
 	print ('##########################################')
@@ -15,16 +15,26 @@ def banner():
 
 banner()
 
+# Function to start metadata extraction
 def printMeta(filename):
-	pdfFile = PdfFileReader(open(filename, 'rb'))
-	docInfo = pdfFile.getDocumentInfo()
-	print('[*] PDF MetaData for: ' + str(filename))
-	for metaItem in docInfo:
-		print('[+] ' + metaItem + ': ' + docInfo[metaItem])
+	try:
+		pdfFile = PdfFileReader(open(filename, 'rb'))
+		docInfo = pdfFile.metadata
+		
+		print(f'[*] PDF MetaData for: {filename}')
+		if docInfo:
+			for metaItem, value in docInfo.items():
+				print(f'[+] {metaItem}: {value}')
+		else:
+			print('[!] No metadata found in this PDF.')	
+	except FileNotFoundError:
+		print(f"Error: File '{filename}' is not found.")
+	except Exception as e:
+		print(f"An error occurred: {e}")
 
 def main():
 	parser = argparse.ArgumentParser(description='Usage program: -F <PDF file name>')
-	parser.add_argument('-F', dest='fileName', type='str', help='Please specify the PDF File name')
+	parser.add_argument('-F', dest='fileName', type=str, help='Please specify the PDF File name')
 	args = parser.parse_args()
 	fileName = args.fileName
 	if not fileName:
